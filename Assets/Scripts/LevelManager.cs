b84fb77b48b3f1bd05dfb34cf11f963d51d3a2b1
Adrian Standowicz <adrian.standowicz@gmail.com>
2018-05-11 14:57:14 +0200

    Modified and applied coding style rules for cleaner coding

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,13 +5,13 @@ public class LevelManager : MonoBehaviour
 {
     public void LoadLevel(string name)
     {
-        Debug.Log ("New Level load: " + name);
+        Debug.Log("New Level load: " + name);
         SceneManager.LoadScene(name);
     }
 
     public void QuitRequest()
     {
-        Debug.Log ("Quit requested");
-        Application.Quit ();
+        Debug.Log("Quit requested");
+        Application.Quit();
     }
 }
--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class MusicPlayer : MonoBehaviour {
-	static MusicPlayer instance = null;
-	
-	void Start () {
-		if (instance != null && instance != this) {
-			Destroy (gameObject);
-			print ("Duplicate music player self-destructing!");
-		} else {
-			instance = this;
-			GameObject.DontDestroyOnLoad(gameObject);
-		}
-		
-	}
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer instance = null;
+
+    private void Start()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            print("Duplicate music player self-destructing!");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(this.gameObject);
+        }
+
+    }
 }
