745658ce7e63b815859b9b5181f14025a7961de6
Adrian Standowicz <adrian.standowicz@gmail.com>
2018-05-20 11:59:40 +0200

    Added missing Score keeper's behaviour.

---
+++ Assets/ScoreKeeper.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public TextMeshProUGUI ScoreText;
+    public static int ScorePoints { get; private set; }
+
+    public void Score(int points)
+    {
+        ScorePoints += points;
+        ScoreText.text = ScorePoints.ToString();
+    }
+
+    public void Reset()
+    {
+        ScorePoints = 0;
+        ScoreText.text = ScorePoints.ToString();
+    }
+
+    private void Start()
+    {
+        if (ScorePoints > 0)
+        {
+            ScoreText.text = ScorePoints.ToString();
+        }
+    }
+}
